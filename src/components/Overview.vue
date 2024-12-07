ba33b6cb8de750a54c8b0f3663d8f1423234f508
ValeaLolo <valeanviolina@gmail.com>
2024-12-07 20:19:59 +0200

    Welcome Back Guys

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -3,7 +3,7 @@
       <v-row>
         <v-col cols="12" sm="4">
           <v-card color="transparent" class="rounded-lg mx-2" flat>
-            <h1 class="white--text">Welcome back Nume </h1>
+            <h1 class="white--text">Welcome back Guys </h1>
             <h5 class="grey--text">We are happy you are <br> motivated to come back </h5>
           </v-card>
         </v-col>
